c1750e902a7553a0d15339101de5b77c11d86a85
manjunathrvce <manjunath180397@gmail.com>
2016-10-12 19:52:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,12 +5,12 @@ var element=document.getElementById('main-text');
 var marginLeft = 0;
 
 function moveRight(){
-  marginLeft=marginLeft+10;
+  marginLeft=marginLeft+1;
   image.style.marginLeft=marginLeft+'px';
 }
 
 
 var image=document.getElementById('madi');
     image.onclick=function(){
-        var interval = setInterval(moveRight,10);
+        var interval = setInterval(moveRight,50);
     };
