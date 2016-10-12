b5d85e5c21cd2dcc8af3ede10645dbd9d8c92e51
manjunathrvce <manjunath180397@gmail.com>
2016-10-12 19:41:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,7 @@ console.log('Loaded!');
 // changes the main text to new value
 var element=document.getElementById('main-text');
     element.innerHTML='New Value';
+var image=document.getElementById('madi');
+    image.onclick=function(){
+      image.style.marginLeft="100px";  
+    };
