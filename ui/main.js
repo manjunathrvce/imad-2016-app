fafccdb5d17055ae9529f1ea128476dcd181c68a
manjunathrvce <manjunath180397@gmail.com>
2016-10-12 19:18:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 console.log('Loaded!');
 // changes the main text to new value
 
-var element=document.getElementbyId('main-text');
-element.innerHTML='New Value';
+var element=document.getElementById('main-text');
+element.innerHTML='New Value'
