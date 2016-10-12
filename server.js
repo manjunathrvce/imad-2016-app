f86d1864866616cedf1a392afcd12f956a4b571a
manjunathrvce <manjunath180397@gmail.com>
2016-10-12 16:32:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ app.get('/', function (req, res) {
 app.get('/:articleName',function(req,res){
     // :articleName==article-one
     // articles[articlesName] == {} contents for article one
-    var articleName=reqs.params.articleName;
+    var articleName=req.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
 app.get('/ui/style.css', function (req, res) {
