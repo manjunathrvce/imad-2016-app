b454a8cade85bd78e7f21a667738de276fe0acfc
manjunathrvce <manjunath180397@gmail.com>
2016-10-14 15:33:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-console.log('Loaded!');
+/*console.log('Loaded!');
 // changes the main text to new value
 var element=document.getElementById('main-text');
     element.innerHTML='New Value';
@@ -14,3 +14,24 @@ var image=document.getElementById('madi');
     image.onclick=function(){
         var interval = setInterval(moveRight,10);
     };
+    */
+    
+    
+    
+// counter coding
+
+var counter = 0 ;
+button=document.getElementById('counter');
+button.onclick = function(){
+    
+  //make a request to the counter endpoint
+  
+  
+  //capture the response and store it in a varaiable
+  
+  
+  //render the variable in the correct span
+  counter=counter+1;
+  var span = document.getElementById('count');
+  span.innerHTML = counter.toString();
+};
