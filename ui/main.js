43549cfa9bc36898a853f20471cbd14206ddffa9
manjunathrvce <manjunath180397@gmail.com>
2016-10-14 16:23:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -58,7 +58,7 @@ submit.onclick = function(){
     var names=['name 1','name 2','name 3','name 4'];
     var list = '';
     for(var i=0;i<names.length;i++){
-        list += '<li>'+names[i]+',</li>';
+        list += '<li>'+names[i]+'</li>';
     }
     var ul = document.getElementById('namelist');
     ul.innerHTML = list;
